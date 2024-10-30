d4adfa8562eb25a35e96892e06052b03e885bebb
dani001024 <aj9473@naver.com>
2024-10-30 17:12:02 +0900

    feat: login 페이지 퍼블리싱

---
+++ apps/client/src/app/(member)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginFrame from '../../../components/pages/auth/LoginFrame';
+
+export default function page() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <LoginFrame />
+    </main>
+  );
+}
