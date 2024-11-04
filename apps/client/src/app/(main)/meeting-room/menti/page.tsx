025e55931c105f80af98743f4ff27a1ca4a02543
snow <cksdn0716@naver.com>
2024-11-04 11:14:15 +0900

    feat: menti 페이지 init

---
+++ apps/client/src/app/(main)/meeting-room/menti/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import MeetingRoomLayOut from '../../../../components/pages/main/meeting-room/menti/MeetingRoomLayOut';
+function page() {
+  return <MeetingRoomLayOut />;
+}
+
+export default page;
---
+++ apps/client/src/components/pages/main/meeting-room/menti/MeetingRoomLayOut.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React from 'react';
+import MeetingRoomNav from '../../../../aside/metting-room/MeetingRoomNav';
+import useFunnel from '../../../../common/Funnel/useFunnel';
+import Meeting from '../mentor/meeting/Meeting';
+
+const steps = [
+  'Overview',
+  'Meeting',
+  'Messages',
+  'Volt',
+  'My Course',
+  'Setting',
+  'Help',
+  'Log Out',
+];
+function MeetingRoomLayOut() {
+  return <div>멘티 네브바 레이아웃 입니다</div>;
+}
+
+export default MeetingRoomLayOut;
