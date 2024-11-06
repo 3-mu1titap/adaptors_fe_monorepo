a98180c7b85634f2718922d5f047f3f88e3b2192
dani001024 <aj9473@naver.com>
2024-11-06 14:51:35 +0900

    feat: 회원가입 API 연결

--- apps/client/src/actions/auth/auth.ts
+++ apps/client/src/actions/auth/auth.ts
@@ -1,1 +1,22 @@
 'use server';
+
+import { commonResType } from '../../components/types/ResponseTypes';
+import { fetchData } from '../common/common';
+
+export const postUserData = async (userData: {
+  name: string;
+  nickName: string;
+  email: string;
+  accountId: string;
+  password: string;
+  phoneNumber: string;
+  role: string;
+}): Promise<any> => {
+  const data = await fetchData<commonResType<any>>({
+    method: 'POST',
+    apiUrl: `/api/v1/auth/sign-up`,
+    body: userData,
+  });
+  console.log(data);
+  return data;
+};
