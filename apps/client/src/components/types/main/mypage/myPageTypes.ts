279bc5421c70b1f9894d0ccb6dde9bc516652b05
Andrea <wjdgnstjr45@gmail.com>
2024-12-03 13:42:55 +0900

    feat: mentor mypage volt api connect

---
+++ apps/client/src/components/types/main/mypage/myPageTypes.ts
@@ -0,0 +1,29 @@
+export interface dateRange {
+  from: Date;
+  to: Date;
+}
+
+export interface exchangeDataType {
+  id: number;
+  date: string;
+  volt: number;
+  status: string;
+  money: number;
+}
+
+export interface settleListDataType {
+  totalExchange: number;
+  exchangeList: exchangeDataType[];
+}
+
+export interface mentorVoltDataType {
+  id: number;
+  volt: number;
+  date: Date;
+  sender: string;
+}
+
+export interface mentorVoltListDataType {
+  totalVolt: number;
+  voltList: mentorVoltDataType[];
+}
