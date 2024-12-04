6fc05abd4032d608a4aeb6d73a3dbce7973e2a42
snow <cksdn0716@naver.com>
2024-12-04 11:35:50 +0900

    feat: 결제 페이지 퍼블리싱

--- apps/web/src/components/pages/payment/payment.tsx
+++ apps/web/src/components/pages/payment/payment.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@repo/ui/components/ui/button';
 import {
   Card,
@@ -20,31 +20,38 @@ function Payment() {
     { id: 5, count: 500, price: 50000 },
   ];
 
+  //선택한 아이템
   const [selectedItem, setSelectedItem] = useState<BoltItem | null>(null);
   //아이템 갯수 최소 1개
-  const [Quantity, setQuantity] = useState(10);
-
+  const [Quantity, setQuantity] = useState(0);
   //총 갯수
-  const [totalCount, setTotalCoutnt] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const [totalMoney, setTotalMoney] = useState(0);
   const handleItemSelect = (item: BoltItem) => {
+    // 다른 아이템을 클릭하면 상태 초기화
+    if (selectedItem?.id !== item.id) {
+      setQuantity(0); // 기본 수량으로 초기화
+      setTotalCount(0); // 총 개수 초기화
+      setTotalMoney(0); // 총 금액 초기화
+    }
+    // 선택된 아이템 업데이트
     setSelectedItem(item);
   };
 
   const decreaseCount = () => {
-    if (Quantity > 10) {
+    if (Quantity > 0) {
       setQuantity(Quantity - 10);
     }
-
-    let total = Quantity - (selectedItem?.count || 0);
-    setTotalCoutnt(total);
   };
 
   const IncreaseCount = () => {
     setQuantity(Quantity + 10);
-
-    let total = Quantity + (selectedItem?.count || 0);
-    setTotalCoutnt(total);
   };
+
+  useEffect(() => {
+    setTotalCount(Quantity + (selectedItem?.count || 0));
+    setTotalMoney(totalCount * 100);
+  }, [Quantity, selectedItem?.count, totalCount]);
   return (
     <div className="flex flex-col w-[30rem] h-[40rem] items-center justify-center">
       <Card className="flex flex-col w-full max-w-[30rem] max-h-[40rem] mx-auto border-2">
@@ -60,7 +67,7 @@ function Payment() {
                 key={item.id}
                 onClick={() => handleItemSelect(item)}
                 variant={selectedItem?.id === item.id ? 'default' : 'outline'}
-                className={`w-full ${selectedItem?.id === item.id ? 'bg-blue-500 text-white' : ''}`}
+                className={`w-full bg-white text-black ${selectedItem?.id === item.id ? 'bg-blue-500 hover:bg-blue-500 text-white' : 'bg-white text-black'}`}
               >
                 {item.count}개 ({item.price.toLocaleString()}원)
               </Button>
@@ -68,7 +75,7 @@ function Payment() {
           </div>
 
           <div className="mt-6">
-            <h3 className="text-lg font-semibold mb-2">결제 수단</h3>
+            <h3 className="text-lg font-semibold mb-6">결제 수단</h3>
             <div className="flex items-center space-x-2 bg-yellow-50 p-3 rounded-md">
               <span className="text-sm font-medium">카카오페이</span>
             </div>
@@ -76,7 +83,7 @@ function Payment() {
 
           {selectedItem && (
             <div className="flex justify-between mt-6 items-center">
-              <div className="flex items-center p-4 justify-start">
+              <div className="flex items-center p-4 justify-start space-x-1">
                 <button onClick={decreaseCount} className="border border-black">
                   {'<'}
                 </button>
@@ -88,16 +95,24 @@ function Payment() {
 
               <div className="flex justify-end">
                 <span className="font bold text-black">총 개수</span>
-                <span>: {selectedItem.count}</span>
+                <span>: {totalCount}</span>
               </div>
             </div>
           )}
         </CardContent>
-        <CardFooter className="flex justify-between items-center">
-          <span className="text-lg font-semibold">총 결제 금액:</span>
-          <span className="text-2xl font-bold text-green-600">
-            {selectedItem ? selectedItem.price.toLocaleString() : '0'}원
-          </span>
+        <CardFooter className="flex flex-col ">
+          <div className="flex justify-between items-center space-x-10 mb-6">
+            <span className="text-lg font-semibold">총 결제 금액:</span>
+            <span className="text-2xl font-bold text-green-600">
+              {selectedItem ? totalMoney.toLocaleString() : '0'}원
+            </span>
+          </div>
+
+          <div>
+            <Button className="bg-yellow-200 text-2xl text-black py-3">
+              결제
+            </Button>
+          </div>
         </CardFooter>
       </Card>
     </div>
