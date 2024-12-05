bbf1f463559a84dda36b906520ef8a76e6ee5371
snow <cksdn0716@naver.com>
2024-12-05 09:32:46 +0900

    fix: 결제 api 수정중

--- apps/web/src/components/pages/payment/payment.tsx
+++ apps/web/src/components/pages/payment/payment.tsx
@@ -1,6 +1,14 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import {
+  PaymentReadyResType,
+  PaymentReqType,
+} from '@components/types/payment/paymentType';
 import { Button } from '@repo/ui/components/ui/button';
+import { PaymentReq } from 'src/actions/payment/paymentActions';
+import { PaymentApproval } from 'src/actions/payment/paymentActions';
+import Link from 'next/link';
 import {
   Card,
   CardHeader,
@@ -10,31 +18,30 @@ import {
 } from '@repo/ui/components/ui/card';
 import TextTitleSection from '../main/mypage/compoent/TextTitleSection';
 import { BoltItem } from '@components/types/payment/paymentType';
+
 function Payment() {
   const BoltItem = [
-    { id: 0, count: 10, price: 1000 },
-    { id: 1, count: 30, price: 3000 },
-    { id: 2, count: 50, price: 5000 },
-    { id: 3, count: 100, price: 10000 },
-    { id: 4, count: 300, price: 30000 },
-    { id: 5, count: 500, price: 50000 },
+    { id: 0, itemName: 'Volt', count: 10, price: 1000 },
+    { id: 1, itemName: 'Volt', count: 30, price: 3000 },
+    { id: 2, itemName: 'Volt', count: 50, price: 5000 },
+    { id: 3, itemName: 'Volt', count: 100, price: 10000 },
+    { id: 4, itemName: 'Volt', count: 300, price: 30000 },
+    { id: 5, itemName: 'Volt', count: 500, price: 50000 },
   ];
-
-  //선택한 아이템
+  const searchParams = useSearchParams();
   const [selectedItem, setSelectedItem] = useState<BoltItem | null>(null);
-  //아이템 갯수 최소 1개
   const [Quantity, setQuantity] = useState(0);
-  //총 갯수
   const [totalCount, setTotalCount] = useState(0);
   const [totalMoney, setTotalMoney] = useState(0);
+  const [Res, setRes] = useState<PaymentReadyResType | undefined>();
+  const [pgToken, setPgToken] = useState('');
+
   const handleItemSelect = (item: BoltItem) => {
-    // 다른 아이템을 클릭하면 상태 초기화
     if (selectedItem?.id !== item.id) {
-      setQuantity(0); // 기본 수량으로 초기화
-      setTotalCount(0); // 총 개수 초기화
-      setTotalMoney(0); // 총 금액 초기화
+      setQuantity(0);
+      setTotalCount(0);
+      setTotalMoney(0);
     }
-    // 선택된 아이템 업데이트
     setSelectedItem(item);
   };
 
@@ -48,10 +55,70 @@ function Payment() {
     setQuantity(Quantity + 10);
   };
 
+  const PaymentApprove = async (pgToken: string) => {
+    if (!Res) return;
+
+    const data = {
+      cid: 'TC0ONETIME',
+      tid: Res.tid || '',
+      partnerOrderId: Res.partnerOrderId || '',
+      partnerUserId: '23993e78-b0af-42de-839f-060ae99a8bf5',
+      pgToken: pgToken,
+      quantity: totalCount || 0,
+    };
+
+    try {
+      console.log('Start');
+
+      const approvalRes = await PaymentApproval(data);
+      console.log('결제 승인 성공:', approvalRes);
+    } catch (error) {
+      console.error('결제 승인 실패:', error);
+    }
+  };
+
   useEffect(() => {
+    //결제 리다이랙트시 pg_token 값을 찾는다
+    const pgToken = searchParams.get('pg_token');
+
+    //pg_token값이 있을경우
+    if (pgToken) {
+      PaymentApprove(pgToken);
+    }
+
     setTotalCount(Quantity + (selectedItem?.count || 0));
     setTotalMoney(totalCount * 100);
-  }, [Quantity, selectedItem?.count, totalCount]);
+  }, [Quantity, selectedItem, totalCount, pgToken]);
+
+  const Paymenthandle = async () => {
+    const data = {
+      cid: 'TC0ONETIME',
+      partnerOrderId: 'string',
+      partnerUserId: '23993e78-b0af-42de-839f-060ae99a8bf5',
+      itemName: selectedItem?.itemName || ' ',
+      quantity: totalCount || 0,
+      totalAmount: totalMoney || 0,
+      taxFreeAmount: 0,
+      approvalUrl: 'https://lims-dev.tistory.com',
+      failUrl: 'https://pknustu.youngjin.com',
+      cancelUrl: 'https://github.com',
+    };
+
+    try {
+      const res = await PaymentReq(data);
+      console.log(res, '결제 준비 완료!!');
+      console.log(res?.nextRedirectPcUrl, 'url url ');
+      console.log(Res, 'useState 데이터');
+
+      if (res?.nextRedirectPcUrl) {
+        window.location.href = res.nextRedirectPcUrl;
+        console.log('이동합니다!~!~');
+      }
+    } catch (error) {
+      console.error('결제 준비 요청 실패:', error);
+    }
+  };
+
   return (
     <div className="flex flex-col w-[30rem] h-[40rem] items-center justify-center">
       <Card className="flex flex-col w-full max-w-[30rem] max-h-[40rem] mx-auto border-2">
@@ -67,20 +134,12 @@ function Payment() {
                 key={item.id}
                 onClick={() => handleItemSelect(item)}
                 variant={selectedItem?.id === item.id ? 'default' : 'outline'}
-                className={`w-full bg-white text-black ${selectedItem?.id === item.id ? 'bg-blue-500 hover:bg-blue-500 text-white' : 'bg-white text-black'}`}
+                className={`w-full ${selectedItem?.id === item.id ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}
               >
                 {item.count}개 ({item.price.toLocaleString()}원)
               </Button>
             ))}
           </div>
-
-          <div className="mt-6">
-            <h3 className="text-lg font-semibold mb-6">결제 수단</h3>
-            <div className="flex items-center space-x-2 bg-yellow-50 p-3 rounded-md">
-              <span className="text-sm font-medium">카카오페이</span>
-            </div>
-          </div>
-
           {selectedItem && (
             <div className="flex justify-between mt-6 items-center">
               <div className="flex items-center p-4 justify-start space-x-1">
@@ -92,7 +151,6 @@ function Payment() {
                   {'>'}
                 </button>
               </div>
-
               <div className="flex justify-end">
                 <span className="font bold text-black">총 개수</span>
                 <span>: {totalCount}</span>
@@ -107,9 +165,11 @@ function Payment() {
               {selectedItem ? totalMoney.toLocaleString() : '0'}원
             </span>
           </div>
-
           <div>
-            <Button className="bg-yellow-200 text-2xl text-black py-3">
+            <Button
+              className="bg-yellow-200 text-2xl text-black py-3 hover:bg-yellow-200"
+              onClick={Paymenthandle}
+            >
               결제
             </Button>
           </div>
