10c38a1fb919cdfca8b97e98d6749d78099d336f
dani001024 <aj9473@naver.com>
2024-12-06 02:47:37 +0900

    chore: 푸터 내용 수정

--- apps/web/src/app/(main)/mentoring/page.tsx
+++ apps/web/src/app/(main)/mentoring/page.tsx
@@ -1,6 +1,5 @@
 import CategoryAside from '@components/pages/main/mentoring/category/CategoryAside';
 import MentoringItem from '@components/pages/main/mentoring/MentoringItem';
-import FitImage from '@components/ui/image/fit-image';
 import { redirect } from 'next/navigation';
 import { getTopCategoryList } from 'src/actions/category/getCategory';
 import { GetMentoringByCategory } from 'src/actions/mentoring/getMentoringList';
@@ -20,6 +19,7 @@ export default async function page({
   console.log(categorise);
   return (
     <main className="mt-[7rem] lg:flex relative lg:mx-4">
+      {/* <PaperStock /> */}
       <CategoryAside categoryParam={searchParams.category} />
       {data && data.length > 0 ? (
         <section className="mx-auto mobile:w-full mobile:px-10 md:px-16 lg:px-36 grid mobile:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mb-10">
@@ -30,13 +30,13 @@ export default async function page({
       ) : (
         <div
           className="min-w-64 min-h-svh mx-auto text-center"
-          style={{ height: `calc(100vh - 456.51px)` }}
+          style={{ height: `calc(100% - 456.51px)` }}
         >
-          <FitImage
+          {/* <FitImage
             src="https://adaptors-bucket.s3.ap-northeast-2.amazonaws.com/mentoring/1733400950295-image-removebg-preview.png"
             alt="볼팡이"
-            className="w-40 h-40"
-          />
+            className="w-40 h-40 "
+          /> */}
           <h1>데이터가 없습니다.</h1>
         </div>
       )}
--- apps/web/src/components/pages/main/mentoring/category/CategoryAside.tsx
+++ apps/web/src/components/pages/main/mentoring/category/CategoryAside.tsx
@@ -1,3 +1,4 @@
+import Thunder from '@components/assets/icons/Thunder';
 import Link from 'next/link';
 import { getTopCategoryList } from 'src/actions/category/getCategory';
 
@@ -12,7 +13,7 @@ export default async function CategoryAside({
       <ul className="lg:min-w-36 pt-10 flex lg:block justify-center lg:justify-normal">
         {categorise?.map((category) => (
           <li
-            className="text-md mobile:text-lg w-full text-center lg:text-start lg:px-10 mb-10"
+            className="text-md mobile:text-lg w-full text-center lg:text-start lg:px-10 mb-10 relative"
             key={category.id}
           >
             <Link
@@ -21,6 +22,9 @@ export default async function CategoryAside({
             >
               {category.topCategoryName}
             </Link>
+            <Thunder
+              className={`${category.topCategoryCode == categoryParam ? `absolute top-[-15px] right-4` : `hidden`}`}
+            />
           </li>
         ))}
       </ul>
