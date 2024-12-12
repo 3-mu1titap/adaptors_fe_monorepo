c3ffc52159073cbe3049797423c59b697b249cb9
Andrea <wjdgnstjr45@gmail.com>
2024-12-12 15:45:59 +0900

    feat: chat server api 연결 및 채팅방 리스트 연결

--- apps/admin/src/app/(main)/mentor/meeting/page.tsx
+++ apps/admin/src/app/(main)/mentor/meeting/page.tsx
@@ -7,45 +7,6 @@ export const metadata: Metadata = {
   title: `Mentoring Meeting`,
 };
 
-// const mentoringSessionList = [
-//   {
-//     mentoringName: '프로그래밍 기초 멘토링',
-//     sessionUuid: 'ac419217-cb98-4334-8b78-8126aa0e57aa',
-//     startDate: '2024-12-04',
-//     endDate: '2024-12-04',
-//     startTime: {
-//       hour: 9,
-//       minute: 0,
-//       second: 0,
-//       nano: 0,
-//     },
-//     endTime: {
-//       hour: 18,
-//       minute: 0,
-//       second: 0,
-//       nano: 0,
-//     },
-//   },
-//   {
-//     mentoringName: '파이썬 기초 멘토링',
-//     sessionUuid: 'ac419217-cb98-4334-8b78-8126aa0e57aa',
-//     startDate: '2024-12-04',
-//     endDate: '2024-12-04',
-//     startTime: {
-//       hour: 9,
-//       minute: 0,
-//       second: 0,
-//       nano: 0,
-//     },
-//     endTime: {
-//       hour: 18,
-//       minute: 0,
-//       second: 0,
-//       nano: 0,
-//     },
-//   },
-// ];
-
 export default async function Page() {
   const mentoringSessionList = await GetTodayMentoringSessionList(
     getDate({ type: false })
