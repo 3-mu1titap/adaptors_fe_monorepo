45475e9d8f9f96633d1c55d45ff57e7b37356146
Andrea <wjdgnstjr45@gmail.com>
2024-12-14 18:30:49 +0900

    style: 볼트 환전 페이지 변경

--- apps/admin/src/app/(main)/mentor/volt/page.tsx
+++ apps/admin/src/app/(main)/mentor/volt/page.tsx
@@ -1,10 +1,18 @@
 import { GetMentorVolts } from '@repo/admin/actions/volt/voltAction';
 import ReceivedVolts from '@repo/admin/components/pages/main/mentor/volt/ReceivedVolts';
 import { mentorVoltListDataType } from '@repo/admin/components/types/main/mypage/myPageTypes';
+import ExchangeVolts from '@repo/admin/components/pages/main/mentor/volt/ExchangeVolts';
 
 export default async function Page() {
   const mentorVoltList: mentorVoltListDataType = await GetMentorVolts();
   return (
-    <>{mentorVoltList && <ReceivedVolts mentorVoltList={mentorVoltList} />}</>
+    <>
+      {mentorVoltList && (
+        <section className="w-full">
+          <ReceivedVolts mentorVoltList={mentorVoltList} />
+          <ExchangeVolts mentorVoltList={mentorVoltList} />
+        </section>
+      )}
+    </>
   );
 }
