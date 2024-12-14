c265df3b684981cf7e78a7ec472f9a1433bfe679
dani001024 <aj9473@naver.com>
2024-12-14 20:56:44 +0900

    feat: auth middlware 설정 및 admin repo 로그인 추가

---
+++ packages/ui/src/components/member/LoginForm.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Input } from '../ui/input';
+import IdAndPassword from './IdAndPassword';
+import NextButton from './NextButton';
+import PasswordViewer from './PasswordViewer';
+
+export default function LoginForm() {
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const id = formData.get('id') as string;
+    const pw = formData.get('password') as string;
+
+    try {
+      const result = await signIn('credentials', {
+        loginId: id,
+        password: pw,
+        redirect: false,
+      });
+      if (result?.status == 401) {
+        setLoginError('아이디 혹은 비밀번호가 일치하지 않습니다.');
+      } else if (result?.status == 200) {
+        console.log(result?.status == 200);
+        router.push('/home');
+      }
+    } catch (error) {
+      console.error(error);
+      setLoginError('로그인 요청 중 오류가 발생했습니다.');
+    }
+  };
+
+  return (
+    <form className="space-y-2 pb-2" onSubmit={handleSubmit}>
+      <Input placeholder="아이디" type="text" name="id" />
+      <fieldset className="relative mb-2">
+        <Input
+          placeholder="패스워드"
+          type={showPassword ? 'text' : 'password'}
+          name="password"
+        />
+        <PasswordViewer
+          isTrue={showPassword}
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      </fieldset>
+      <IdAndPassword />
+      {loginError && <p className="text-red-500 text-sm">{loginError}</p>}
+      <NextButton
+        text="로그인"
+        colorType="primary"
+        textColor="text-white"
+        type="submit"
+      />
+    </form>
+  );
+}
