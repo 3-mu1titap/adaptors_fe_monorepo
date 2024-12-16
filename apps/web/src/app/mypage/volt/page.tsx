b6555714baff52e898d2ff457cf3cd50da978c6c
snow <cksdn0716@naver.com>
2024-12-16 18:25:09 +0900

    fix: 결제 페이지 수정 초안

--- apps/web/src/app/mypage/volt/page.tsx
+++ apps/web/src/app/mypage/volt/page.tsx
@@ -6,59 +6,68 @@ import {
 } from '@repo/ui/components/ui/tabs';
 import VoltCharge from '@repo/web/components/pages/main/mypage/volt/Charge/VoltCharge';
 import VoltUsageList from '@repo/web/components/pages/main/mypage/volt/VoltUsageList';
-import MentorTitleSection from '@repo/web/components/pages/mentor/compoent/MentorTitleSection';
 
 import {
   GetMemberPoint,
   GetPointList,
 } from 'src/actions/payment/paymentActions';
+
 async function page() {
-  //회원의 남은 포인트 조회
+  // 회원의 남은 포인트 조회
   const res = await GetMemberPoint();
-  // console.log(res, '회원 포인트 조회 성공');
 
   const data = await GetPointList(0);
-  // console.log(data, '포인트 리스트 조회 성공');
 
   return (
     <>
-      <div className="container mx-auto lg:max-w-full md:max-w-[50rem] moblie:max-w-[400px] max-w-[300px] bg-gray-100 h-full">
+      <div className="container mx-auto lg:max-w-full md:max-w-[50rem] mobile:max-w-[400px] max-w-[300px] bg-gray-100 h-full relative">
         <div className="flex flex-col py-8 mt-7 mx-auto lg:max-w-full">
-          <span
-            className="ml-14 text-black font-bold text-3xl mb-10"
-            title="My Volt"
-          >
-            My Volt
-          </span>
-          <Tabs defaultValue="Charge">
-            <TabsList className="shadow-2xl bg-gray-300">
-              <TabsTrigger className="hover:bg-black" value="Charge">
-                Charge
-              </TabsTrigger>
-              <TabsTrigger value="ChargeList">Charge List</TabsTrigger>
-            </TabsList>
+          {/* 볼트 페이지 헤더 */}
+          <div className="flex items-center justify-between mb-6">
+            <span
+              className="ml-20 text-black font-bold text-4xl"
+              title="My Volt"
+            >
+              My Volt
+            </span>
+          </div>
 
-            <TabsContent value="Charge">
-              {/* 볼츠 충전 컴포넌트 */}
-              <span className="text-black text-xl ml-14 mt-11 font-bold text-center">
-                볼트 충전
-              </span>
-              <VoltCharge points={res?.result || undefined} />
-            </TabsContent>
-            <TabsContent value="ChargeList">
-              <span className="text-black text-xl ml-14 font-bold text-center">
-                볼트 내역
-              </span>
-              <ul className="flex flex-col underline-offset-1 gap-y-4 just h-auto">
-                {(data && (
-                  <VoltUsageList
-                    total={data?.result.totalPage}
-                    item={data.result.paymentResponseDtoList}
-                  />
-                )) ||
-                  null}
-              </ul>
-            </TabsContent>
+          <Tabs defaultValue="Charge">
+            {/* Tabs List Positioned in Top-Right */}
+            <div className="absolute top-5 right-8 max-w-[250px] items-center">
+              <TabsList className="shadow-lg bg-gray-300 rounded-md flex space-y-2">
+                <TabsTrigger
+                  className="px-3 py-2 text-lg hover:bg-black hover:text-white rounded-md"
+                  value="Charge"
+                >
+                  Charge
+                </TabsTrigger>
+                <TabsTrigger
+                  className="px-3 py-2 text-lg hover:bg-black hover:text-white rounded-md"
+                  value="ChargeList"
+                >
+                  Charge List
+                </TabsTrigger>
+              </TabsList>
+            </div>
+
+            {/* Tabs Content */}
+            <div className="mt-10 w-full">
+              <TabsContent value="Charge">
+                <VoltCharge points={res?.result || undefined} />
+              </TabsContent>
+              <TabsContent value="ChargeList">
+                <ul className="flex flex-col underline-offset-1 gap-y-4 h-auto">
+                  {(data && (
+                    <VoltUsageList
+                      total={data?.result.totalPage}
+                      item={data.result.paymentResponseDtoList}
+                    />
+                  )) ||
+                    null}
+                </ul>
+              </TabsContent>
+            </div>
           </Tabs>
         </div>
       </div>
--- apps/web/src/components/pages/main/mypage/volt/Charge/VoltCharge.tsx
+++ apps/web/src/components/pages/main/mypage/volt/Charge/VoltCharge.tsx
@@ -4,12 +4,14 @@ function VoltCharge({ points }: { points: any }) {
   return (
     <div className="flex  max-w-[64rem] mx-auto space-x-10 bg-white rounded-2xl mt-10 justify-between border-2 border-gray-200">
       <div className="flex flex-col items-start p-10 gap-y-3 ">
-        <span className="text-2xl font-bold text-black">남은 볼트</span>
+        <span className="text-xl font-semibold text-black">
+          사용 가능한 볼트 수
+        </span>
 
         <span className="text-4xl font-bold">{points} 볼트</span>
       </div>
 
-      <div className="flex mr-10 justify-center items-center">
+      <div className="flex mr-42 justify-center items-center">
         <Link href="/payment">
           <Button className="flex mr-56 bg-yellow-200 self-center hover:bg-black hover:text-white text-black py-4">
             충전
