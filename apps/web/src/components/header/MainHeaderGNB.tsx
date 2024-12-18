e1407763895634ffcfc083806a952738225e01f8
snow <cksdn0716@naver.com>
2024-12-18 19:53:10 +0900

    fix: 수정

--- apps/web/src/components/header/MainHeaderGNB.tsx
+++ apps/web/src/components/header/MainHeaderGNB.tsx
@@ -3,8 +3,8 @@ import MainHeaderGNBMenuItem from './MainGNBMenuItem';
 
 function MainHeaderGNB() {
   return (
-    <nav className="hidden md:visible justify-center text-sm ml-2 md:flex sm:flex sm:justify-center md:justify-center lg:flex w-full">
-      <ul className="text-base lg:text-lg flex gap-x-4 lg:gap-x-8">
+    <nav className="block md:visible justify-center text-sm ml-2 md:flex sm:flex sm:justify-center md:justify-center lg:flex w-full">
+      <ul className="text-base  justify-center lg:text-lg flex gap-x-2 md:gap-x-12 lg:gap-x-8">
         {headerGNBMenuData.map((item) => (
           <MainHeaderGNBMenuItem key={item.label} menuItem={item} />
         ))}
